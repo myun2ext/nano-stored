41a1bb7180ad2c29746939b0106431f6afccb1c7
myun2 <myun2@nwhite.info>
2014-08-21 18:35:26 +0900

    Update nano_stored.h
    entry structure add

--- include/nano_stored.h
+++ include/nano_stored.h
@@ -1,6 +1,14 @@
 #ifndef __MYUN2_GITHUB__NANO_STORED_H__
 #define __MYUN2_GITHUB__NANO_STORED_H__
 
-typedef unsigned long entry_data_t;
+#define NS_BLOCK_LENGTH  (16)
+
+typedef unsigned long ns_entry_data_t;
+
+/* Entry Type structure */
+typedef struct
+{
+  ns_entry_data_t entries[NS_BLOCK_LENGTH];
+} ns_record_t, ns_entry_t;
 
 #endif//__MYUN2_GITHUB__NANO_STORED_H__
