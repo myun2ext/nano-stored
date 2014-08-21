4e1ab0c0e3f6e0be0d4e69be7c89ba7d47925bf1
myun2 <myun2@nwhite.info>
2014-08-21 19:12:18 +0900

    Update types.h
    string, float, double type Add.

--- include/types.h
+++ include/types.h
@@ -2,14 +2,19 @@
 #define __MYUN2_GITHUB__NANO_STORED__TYPES_H__
 
 enum ns_types {
-  ns_type_null,					/* 0: Null Type */
+  ns_type_null=0,				/* 0: Null Type */
   ns_type_int,					/* 1: Integer Type (32bit) */
   ns_type_char,					/* 2: 1byte(C char like) Type */
-  ns_type_char,					/* 3: 2byte(C short like) Type */
+  ns_type_short,				/* 3: 2byte(C short like) Type */
   ns_type_u_int,				/* 4: Unsigned Integer Type (32bit) */
 
   ns_type_long=6,				/* 6: Long(64bit) Type */
   ns_type_u_long,				/* 7: Unsigned Long(64bit) Type */
+
+  ns_type_string=8,				/* 8: String */
+
+  ns_type_float=16,				/* 16: Float Type (32bit) */
+  ns_type_double,				/* 17: Double Type (64bit) */
 };
 
 #endif//__MYUN2_GITHUB__NANO_STORED__TYPES_H__
