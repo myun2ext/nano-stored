33281f41e3cc57801f06d1d01df18da1a90c30ac
myun2 <myun2@nwhite.info>
2014-08-22 19:13:33 +0900

    Large blocks Structure Add.

--- include/block.h
+++ include/block.h
@@ -21,4 +21,34 @@ typedef struct
   ns_entry_data_t entries[NS_BLOCK_LENGTH * 3];
 } ns_triple_block_t;
 
+/* 16Blocks (chunked) */
+typedef struct
+{
+  ns_entry_data_t entries[NS_BLOCK_LENGTH * 16];
+} ns_large_block_t;
+
+/* 32Blocks */
+typedef struct
+{
+  ns_entry_data_t entries[NS_BLOCK_LENGTH * 32];
+} ns_ll_block_t;
+
+/* 64Blocks (xLarge) */
+typedef struct
+{
+  ns_entry_data_t entries[NS_BLOCK_LENGTH * 64];
+} ns_xl_block_t;
+
+/* 128Blocks (Very Large) */
+typedef struct
+{
+  ns_entry_data_t entries[NS_BLOCK_LENGTH * 128];
+} ns_vl_block_t;
+
+/* 256Blocks (Ultra Very Large) */
+typedef struct
+{
+  ns_entry_data_t entries[NS_BLOCK_LENGTH * 256];
+} ns_uvl_block_t;
+
 #endif//__MYUN2_GITHUB__NANO_STORED__BLOCK_H__
