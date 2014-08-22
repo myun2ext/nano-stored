d4449c40c2c7b96af7c4ebab3ec74cc2ec114a4b
myun2 <myun2@nwhite.info>
2014-08-22 19:24:57 +0900

    rename block structure and expand (quad, medium large)

--- include/block.h
+++ include/block.h
@@ -21,34 +21,45 @@ typedef struct
   ns_entry_data_t entries[NS_BLOCK_LENGTH * 3];
 } ns_triple_block_t;
 
-/* 16Blocks (chunked) */
+typedef struct
+{
+  ns_entry_data_t entries[NS_BLOCK_LENGTH * 4];
+} ns_quad_block_t;
+
+/* 8 Blocks (Medium Large) */
+typedef struct
+{
+  ns_entry_data_t entries[NS_BLOCK_LENGTH * 8];
+} ns_medium_large_block_t;
+
+/* 16Blocks (Large) */
 typedef struct
 {
   ns_entry_data_t entries[NS_BLOCK_LENGTH * 16];
 } ns_large_block_t;
 
-/* 32Blocks */
+/* 32Blocks (Double large) */
 typedef struct
 {
   ns_entry_data_t entries[NS_BLOCK_LENGTH * 32];
-} ns_ll_block_t;
+} ns_double_large_block_t, ns_dl_block_t;
 
 /* 64Blocks (xLarge) */
 typedef struct
 {
   ns_entry_data_t entries[NS_BLOCK_LENGTH * 64];
-} ns_xl_block_t;
+} ns_xl_block_t, ns_xlarge_block_t;
 
 /* 128Blocks (Very Large) */
 typedef struct
 {
   ns_entry_data_t entries[NS_BLOCK_LENGTH * 128];
-} ns_vl_block_t;
+} ns_vl_block_t, ns_very_large_block, ns_very_large_block_t;
 
 /* 256Blocks (Ultra Very Large) */
 typedef struct
 {
   ns_entry_data_t entries[NS_BLOCK_LENGTH * 256];
-} ns_uvl_block_t;
+} ns_uvl_block_t, ns_ultra_very_large_block;
 
 #endif//__MYUN2_GITHUB__NANO_STORED__BLOCK_H__
--- test.c
+++ test.c
@@ -6,16 +6,16 @@
 
 int main()
 {
-  printf("Single Block size:   %lu\n", sizeof(ns_block_t));
-  printf("Double Block size:   %lu\n", sizeof(ns_double_block_t));
-  printf("Triple Block size:   %lu\n", sizeof(ns_triple_block_t));
-  printf("Quad Block size:     %lu\n", sizeof(ns_quad_block_t));
-
-  printf("Medium large block:  %lu\n", sizeof(ns_medium_large_block_t));
-  printf("Large Block size:    %lu\n", sizeof(ns_large_block_t));
-  printf("LL Block size:       %lu\n", sizeof(ns_ll_block_t));
-  printf("XLarge Block size:   %lu\n", sizeof(ns_xl_block_t));
-  printf("Very Large Block:    %lu\n", sizeof(ns_vl_block_t));
-  printf("Ultra Very Large Block: %lu\n", sizeof(ns_uvl_block_t));
+  printf("Single Block size:        %lu\n", sizeof(ns_block_t));
+  printf("Double Block size:        %lu\n", sizeof(ns_double_block_t));
+  printf("Triple Block size:        %lu\n", sizeof(ns_triple_block_t));
+  printf("Quad Block size:          %lu\n", sizeof(ns_quad_block_t));
+  printf("------------------------------\n");
+  printf("Medium large block:       %lu\n", sizeof(ns_medium_large_block_t));
+  printf("Large Block size:         %lu\n", sizeof(ns_large_block_t));
+  printf("Double Large Block size:  %lu\n", sizeof(ns_double_large_block_t));
+  printf("XLarge Block size:        %lu\n", sizeof(ns_xl_block_t));
+  printf("Very Large Block:         %lu\n", sizeof(ns_vl_block_t));
+  printf("Ultra Very Large Block:   %lu\n", sizeof(ns_uvl_block_t));
   return 0;
 }
