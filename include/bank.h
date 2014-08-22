ff956dca185684108f77ea3936b431e26ed6dbaf
myun2ext <myun2@mbm.nifty.com>
2014-08-22 12:13:31 +0900

    Update bank.h
    ns_bank_persistent_method, ns_bank_header_t

--- include/bank.h
+++ include/bank.h
@@ -1,4 +1,16 @@
 #ifndef __MYUN2_GITHUB__NANO_STORED__BANK_H__
 #define __MYUN2_GITHUB__NANO_STORED__BANK_H__
 
+typedef enum {
+  memory,
+  file
+} ns_bank_persistent_method;
+
+typedef struct {
+  ns_bank_persistent_method method;
+} ns_bank_header_t;
+
+typedef struct {
+} ns_bank_t;
+
 #endif//__MYUN2_GITHUB__NANO_STORED__BANK_H__
