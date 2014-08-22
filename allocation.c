e663851d3880a452e81e10a1a971b4630da09f20
myun2 <myun2@nwhite.info>
2014-08-23 05:29:11 +0900

    allocation.c: ns_alloc() implemented

--- allocation.c
+++ allocation.c
@@ -1,5 +1,15 @@
+#include <memory.h>
+
 typedef struct
 {
   void* ptr;
   unsigned long size;
 } ns_memory_ptr, *ns_memory_handle;
+
+ns_memory_handle ns_alloc(unsigned long size)
+{
+  ns_memory_ptr p;
+  p.ptr = malloc(size);
+  p.size = size;
+  return p;
+}
